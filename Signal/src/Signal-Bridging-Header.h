c70e9ae299b67fa9a6e2b4a66f7525ab6df44e69
Michael Kirk <michael.code@endoftheworl.de>
2016-11-12 12:22:29 -0500

    WebRTC calling WIP.
    * Ensure NotificationsManager has dependencies
        Otherwise it's easy to mess up the order of the required dependencies.
    * move AccountManager into Environment, it's heavy to construct
    
    // FREEBIE

--- Signal/src/Signal-Bridging-Header.h
+++ Signal/src/Signal-Bridging-Header.h
@@ -3,10 +3,13 @@
 //
 #import <Foundation/Foundation.h>
 
+#import "AppAudioManager.h"
+#import "AudioController.h"
 #import "Environment.h"
 #import "OWSContactAvatarBuilder.h"
 #import "OWSContactsManager.h"
 #import "OWSLogger.h"
+#import "OWSWebRTCDataProtos.pb.h"
 #import "PhoneNumber.h"
 #import "PropertyListPreferences.h"
 #import "PushManager.h"
@@ -15,13 +18,23 @@
 #import "UIUtil.h"
 #import <SignalServiceKit/Contact.h>
 #import <SignalServiceKit/NSDate+millisecondTimeStamp.h>
+#import <SignalServiceKit/OWSCallAnswerMessage.h>
+#import <SignalServiceKit/OWSCallBusyMessage.h>
+#import <SignalServiceKit/OWSCallHangupMessage.h>
+#import <SignalServiceKit/OWSCallIceUpdateMessage.h>
+#import <SignalServiceKit/OWSCallMessageHandler.h>
+#import <SignalServiceKit/OWSCallOfferMessage.h>
 #import <SignalServiceKit/OWSEndSessionMessage.h>
 #import <SignalServiceKit/OWSError.h>
 #import <SignalServiceKit/OWSMessageSender.h>
+#import <SignalServiceKit/OWSOutgoingCallMessage.h>
+#import <SignalServiceKit/OWSTurnServerInfoRequest.h>
 #import <SignalServiceKit/TSAccountManager.h>
+#import <SignalServiceKit/TSCall.h>
 #import <SignalServiceKit/TSContactThread.h>
 #import <SignalServiceKit/TSErrorMessage.h>
 #import <SignalServiceKit/TSInfoMessage.h>
+#import <SignalServiceKit/TSNetworkManager.h>
 #import <SignalServiceKit/TSStorageManager+IdentityKeyStore.h>
 #import <SignalServiceKit/TSStorageManager+SessionStore.h>
 #import <SignalServiceKit/TSStorageManager+keyingMaterial.h>
